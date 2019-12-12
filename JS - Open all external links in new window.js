f05bc73577f2ec275b3c5b0c9608bdd5563ee025
Rohan Vyas <info.rdcorp@gmail.com>
2019-12-12 17:59:00 +0530

    Create JS - Open all external links in new window.js

---
+++ JS - Open all external links in new window.js
@@ -0,0 +1,11 @@
+/* Open all external links in new window */
+			jQuery('a').each(function() {
+			   var a = new RegExp('/' + window.location.host + '/');
+			   if(!a.test(this.href)) {
+				   jQuery(this).click(function(event) {
+					   event.preventDefault();
+					   event.stopPropagation();
+					   window.open(this.href, '_blank');
+				   });
+			   }
+			});
